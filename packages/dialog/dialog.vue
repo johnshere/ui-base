3c9844fd3086a2284f7de40eea1449d4068851fc
liujiang <liujiang@youzhicai.com>
2023-09-26 14:39:27 +0800

    feat: dialog header字体大小调整，body边距调整

--- packages/dialog/dialog.vue
+++ packages/dialog/dialog.vue
@@ -6,22 +6,33 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.el-dialog__header {
-  width: 100%;
-  height: 34px;
-  background: #f5f7fa;
-  border: 1px solid #dad7e2;
-  border-radius: 2px 2px 0px 0px;
-  line-height: 32px;
-  padding: 0;
-  padding-left: 20px;
-  box-sizing: border-box;
-  .el-dialog__title {
-    font-size: 16px;
+.el-dialog {
+  &__header {
+    width: 100%;
+    height: 38px;
+    background: #f5f7fa;
+    border: 1px solid #dad7e2;
+    border-radius: 2px 2px 0px 0px;
+    line-height: 36px;
+    padding: 0;
+    padding-left: 18px;
+    box-sizing: border-box;
+
+    .el-dialog__title {
+      font-size: 16px;
+    }
+
+    .el-dialog__headerbtn {
+      top: 10px;
+      right: 18px;
+      .el-dialog__close {
+        font-size: 18px;
+      }
+    }
   }
-  .el-dialog__headerbtn {
-    top: 10px;
-    right: 16px;
+
+  &__body {
+    padding: 32px 36px 42px;
   }
 }
 </style>
