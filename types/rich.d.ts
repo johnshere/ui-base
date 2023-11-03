c5a1432b14912fd5715436ce0ed2b3ba9795c6f7
liujiang <liujiang@youzhicai.com>
2023-11-03 11:20:52 +0800

    feat: 增加Title-card、RichEditor、RichView组件和类型

---
+++ types/rich.d.ts
@@ -0,0 +1,12 @@
+import { UComponent } from "./component";
+
+export declare class URichEditor extends UComponent {
+  value: String;
+  placeholder: String;
+  id?: String;
+}
+
+export declare class URichView extends UComponent {
+  value: String;
+}
+
