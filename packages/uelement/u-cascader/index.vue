f8ce4beb104f7c51c2f0f35705335a1b66d72f7f
liujiang <liujiang@youzhicai.com>
2023-11-07 18:36:55 +0800

    feat: DatePicker、Cascader

---
+++ packages/uelement/u-cascader/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-cascader :style="style" v-bind="$attrs" v-on="$listeners">
+    <template v-for="slot in Object.keys($scopedSlots)" :slot="slot">
+      <slot :name="slot" />
+    </template>
+  </el-cascader>
+</template>
+<script>
+export default {
+  name: "UCascader",
+  props: {
+    style: {
+      type: [Object, String],
+      default: () => {
+        return { width: '100%'};
+      },
+    },
+  },
+};
+</script>
---
+++ packages/uelement/u-date-picker/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-date-picker :style="style" v-bind="$attrs" v-on="$listeners">
+    <template v-for="slot in Object.keys($scopedSlots)" :slot="slot">
+      <slot :name="slot" />
+    </template>
+  </el-date-picker>
+</template>
+<script>
+export default {
+  name: "UDatePicker",
+  props: {
+    style: {
+      type: [Object, String],
+      default: () => {
+        return { width: '100%'};
+      },
+    },
+  },
+};
+</script>
