a086016f25c59b46498006037214622aa31aa46b
jingwu <wujing@youzhicai.com>
2023-12-07 16:24:49 +0800

    feat: u-link加loading效果

--- packages/uelement/u-link/u-link.vue
+++ packages/uelement/u-link/u-link.vue
@@ -1,15 +1,39 @@
 <template>
   <el-link
     v-bind="$attrs"
+    :disabled="loading"
     v-on="$listeners"
+    @click="execute"
   >
     <template v-for="slot in Object.keys($scopedSlots)" :slot="slot">
-      <slot :name="slot" />
+      <i v-show="loading" class="el-icon-loading" /><slot :name="slot" />
     </template>
   </el-link>
 </template>
 <script>
 export default {
   name: "ULink",
+  props: {
+    click: {
+      type: Function,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  methods: {
+    execute() {
+      if (this.click) {
+        this.loading = true;
+        const p = this.click();
+        Promise.resolve(p).finally(() => {
+          this.loading = true;
+        });
+      }
+    },
+  },
 };
 </script>
