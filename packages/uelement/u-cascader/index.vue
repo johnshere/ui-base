fb21731fd0f913f647e9f715fcc240d294e64293
jingwu <wujing@youzhicai.com>
2023-12-14 14:48:17 +0800

    fix: cascader写固定高度，解决不同环境下，高度变化问题

--- packages/uelement/u-cascader/index.vue
+++ packages/uelement/u-cascader/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-cascader v-bind="$attrs" ref="cascader" v-on="$listeners">
+  <el-cascader
+    :append-to-body="false"
+    v-bind="$attrs"
+    ref="cascader"
+    v-on="$listeners"
+  >
     <template v-for="slot in Object.keys($scopedSlots)" :slot="slot">
       <slot :name="slot" />
     </template>
@@ -18,5 +23,8 @@ export default {
 <style lang="less" scoped>
 .el-cascader {
   width: 100%;
+}
+::v-deep .el-cascader-menu__wrap {
+  height: 204px;
 }
 </style>
