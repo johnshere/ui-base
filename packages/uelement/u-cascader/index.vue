d8071605b5d984f6ea3b3c7dfa225a0cd9bbf844
jingwu <wujing@youzhicai.com>
2023-12-14 10:42:10 +0800

    fix: cascader加个getCheckedNodes

--- packages/uelement/u-cascader/index.vue
+++ packages/uelement/u-cascader/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-cascader v-bind="$attrs" v-on="$listeners">
+  <el-cascader v-bind="$attrs" ref="cascader" v-on="$listeners">
     <template v-for="slot in Object.keys($scopedSlots)" :slot="slot">
       <slot :name="slot" />
     </template>
@@ -8,6 +8,11 @@
 <script>
 export default {
   name: "UCascader",
+  methods: {
+    getCheckedNodes() {
+      return this.$refs.cascader.getCheckedNodes();
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
