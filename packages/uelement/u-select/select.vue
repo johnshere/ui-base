c3f2a52ef5112c3752ad1be49e4d0b162b27fc5d
liujiang <liujiang@youzhicai.com>
2024-01-11 19:34:24 +0800

    fix: select readonly bugfix

--- packages/uelement/u-select/select.vue
+++ packages/uelement/u-select/select.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-select v-bind="$attrs" v-on="$listeners">
+  <el-select v-bind="$attrs" v-on="_listeners">
     <el-option v-for="(item, i) in list" :key="i" v-bind="item" />
     <template v-for="(_, name) in $scopedSlots" v-slot:[name]="data">
       <slot :name="name" v-bind="data"></slot>
