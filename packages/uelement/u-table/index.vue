52114c07e391bda619bde993ee1b9af078eb033c
jingwu <wujing@youzhicai.com>
2024-02-05 20:41:58 +0800

    fix: u-table, empty优化

--- packages/uelement/u-table/index.vue
+++ packages/uelement/u-table/index.vue
@@ -7,85 +7,92 @@
     v-on="$listeners"
   >
     <template v-for="slot in Object.keys($scopedSlots)" :slot="slot">
-      <slot :name="slot" />
+      <div :key="slot">
+        <div v-if="slot === 'empty'" slot="empty">
+          <div class="u-page-table__empty">
+            <slot :name="slot" />
+          </div>
+        </div>
+        <slot v-else :name="slot" />
+      </div>
     </template>
-    <div slot="empty">
+    <div v-if="!Object.keys($scopedSlots).includes('empty')" slot="empty">
       <div class="u-page-table__empty">暂无数据</div>
     </div>
   </el-table>
 </template>
 <script>
 export default {
-  name: 'UTable',
+  name: "UTable",
   data() {
-    const isAdapter = location.href.includes('isPrint=1') || location.href.includes('isPdf=1')
+    const isAdapter =
+      location.href.includes("isPrint=1") || location.href.includes("isPdf=1");
     return {
-      printClassName: isAdapter ? 'adapter-screen' : ''
+      printClassName: isAdapter ? "adapter-screen" : "",
     };
   },
   methods: {
     toggleRowSelection() {
-      this.$refs.table.toggleRowSelection(...arguments)
-    }
+      this.$refs.table.toggleRowSelection(...arguments);
+    },
   },
 };
 </script>
 <style lang="less" scoped>
-  .el-table ::v-deep {
-    .el-table__cell:last-child {
-      .el-button {
-        margin-left: 0;
-        margin-right: 12px;
-        &.el-button--small {
-          font-size: 14px;
-        }
-        &:last-child {
-          margin-right: 0;
-        }
+.el-table ::v-deep {
+  .el-table__cell:last-child {
+    .el-button {
+      margin-left: 0;
+      margin-right: 12px;
+      &.el-button--small {
+        font-size: 14px;
       }
-      .el-link {
-        vertical-align: unset;
+      &:last-child {
+        margin-right: 0;
       }
     }
-    thead th.el-table__cell {
-      color: #606266;
-      font-weight: bold;
-      background: #f6f7fb;
-    }
-    .el-table__header th {
-      font-weight: bold;
-      background-color: #f5f7fa;
-      color: #333;
-    }
-    // td {
-    //   padding: 5px 0;
-    // }
-    th {
-      padding: 10px 0;
-      &.center {
-        text-align: center;
-      }
+    .el-link {
+      vertical-align: unset;
     }
   }
+  thead th.el-table__cell {
+    color: #606266;
+    font-weight: bold;
+    background: #f6f7fb;
+  }
+  .el-table__header th {
+    font-weight: bold;
+    background-color: #f5f7fa;
+    color: #333;
+  }
+  // td {
+  //   padding: 5px 0;
+  // }
+  th {
+    padding: 10px 0;
+    &.center {
+      text-align: center;
+    }
+  }
+}
 .adapter-screen {
   ::v-deep .el-table__header {
     width: 100% !important;
   }
-  ::v-deep .el-table__header col{
+  ::v-deep .el-table__header col {
     width: 25% !important;
   }
-  ::v-deep .el-table__header col[name="gutter"]{
+  ::v-deep .el-table__header col[name="gutter"] {
     width: 0 !important;
   }
-  ::v-deep .el-table__body{
+  ::v-deep .el-table__body {
     width: 100% !important;
   }
-  ::v-deep .el-table__body col{
+  ::v-deep .el-table__body col {
     width: 25% !important;
   }
-  ::v-deep .el-table__body col[name="gutter"]{
+  ::v-deep .el-table__body col[name="gutter"] {
     width: 0 !important;
   }
 }
-
 </style>
