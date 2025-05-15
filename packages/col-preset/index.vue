9eafc976bb8b9bb9125428e7b13785f1b3d32dc1
liujiang <liujiang@youzhicai.com>
2025-05-15 09:44:07 +0800

    feat: preset

---
+++ packages/col-preset/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-col v-bind="{ ...$attrs, ...preset }">
+        <slot />
+    </el-col>
+</template>
+<script>
+import { Col } from 'element-ui';
+import 'element-ui/packages/theme-chalk/src/col.scss';
+export default {
+  name: 'UColPreset',
+  components: {
+    [Col.name]: Col
+  },
+  inject: ['UForm'],
+  computed: {
+    preset() {
+      const isView = this.UForm?.$attrs.isView
+      const isview = this.UForm?.$attrs['is-view'];
+      if (!!isView || isView === '' || !!isview || isview === '') {
+        return { xxl:6, xl: 6, lg: 8, md: 12, sm: 12 }
+      }
+      return { xxl:8, xl: 8, lg: 8, md: 12, sm: 12 }
+    }
+  }
+};
+</script>
--- packages/uelement/u-form/u-form.vue
+++ packages/uelement/u-form/u-form.vue
@@ -19,6 +19,11 @@ export default {
   components: {
     [Form.name]: Form
   },
+  provide() {
+    return {
+      UForm: this
+    };
+  },
   computed:{
     listeners(){
       return listeners.call(this)
